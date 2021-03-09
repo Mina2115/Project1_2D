559622f5275674e3f753d1a53ef8fc3b8d5eb6bf
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-03-09 10:17:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer"
+      ], function (Map, MapView, FeatureLayer) {
         var map = new Map({
-          basemap: "dark-gray"
+          basemap: "topo-vector"
         });
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
+          zoom: 7,
+          center: [-90, 38]
         });
+
+
+        /********************
+         * Add feature layer 1
+         ********************/
+
+        // Colleges with geospatial related programs in Greater St. Louis Area.
+        var featureLayer_1 = new FeatureLayer({
+  url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Mina_Final_Project_13AUG20_WFL1/FeatureServer"
+});
+
+map.add(featureLayer_1);
+
+  /********************
+         * Add feature layer 2
+         ********************/
+
+        // Boundaries of Missouri's 114 counties plus the boundary of the city of St. Louis
+
+  var featureLayer_2 = new FeatureLayer({
+  url: "https://services2.arcgis.com/kNS2ppBA4rwAQQZy/arcgis/rest/services/MO_2018_County_Boundaries/FeatureServer"
+});
+
+map.add(featureLayer_2);
+
+        map.add(featureLayer_1);
       });
